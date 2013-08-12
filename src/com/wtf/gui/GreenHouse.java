9c8880a1405501dadda8b24c61c66db571d0d04c
Konstrictorman <rikhardho@hotmail.com>
2013-08-12 00:17:11 -0500

    lectura de propiedades

--- src/com/wtf/gui/GreenHouse.java
+++ src/com/wtf/gui/GreenHouse.java
@@ -11,5 +11,62 @@ public class GreenHouse {
 	
 	private static Logger log = Logger.getLogger(GreenHouse.class.getName());
 	public static ResourceBundle props = ResourceBundle.getBundle("greenHouse");
+	
+	private String name;
+	private String address;
+	private String port;
+	
+	public GreenHouse() {
+		name = props.getString("name");
+		address = props.getString("IP");
+		port = props.getString("port");
+	}
+	
+	
+	public static void main(String[] args) {
+		GreenHouse gh = new GreenHouse();
+		log.info("name: " + gh.getName());
+		log.info("Ip: " + gh.getAddress());
+		log.info("port: " + gh.getPort());
+		
+	}
 
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public String getPort() {
+		return port;
+	}
+
+
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	
+	
 }
